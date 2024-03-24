f84e9cff09818cbaa5e4387f4c8cd1bc4775b02b
SerkanTemiz01 <serkan154.gs@gmail.com>
2024-03-25 00:05:54 +0300

    Add project files.

---
+++ CSProjeDemo1/Kutuphane.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSProjeDemo1
+{
+    public class Kutuphane
+    {
+       
+        public List<Kitap> Kitaplar {  get; set; }
+        public List<Uye> Uyeler {  get; set; }
+
+        public Kutuphane()
+        {
+            Kitaplar = new List<Kitap> ();
+            Uyeler = new List<Uye>();
+        }
+
+        public void KitapDurumGuncelle(Kitap kitap,Durum durum)
+        {
+            kitap.Durum=durum;
+        }
+        public void UyeEkle(Uye uye)
+        {
+            Uyeler.Add(uye);
+            Console.WriteLine($"{uye.Ad} {uye.Soyad} isimli üye kütüphaneye eklendi");
+        }
+        public void KitapEkle(Kitap kitap)
+        {
+            Kitaplar.Add(kitap);
+            Console.WriteLine($"{kitap.Baslik} isimli kitap kütüphaneye eklendi");
+        }
+        public List<Kitap> MevcutKitaplarıListele()
+        {
+            return Kitaplar;
+        }
+        public List<Kitap> OduncAlinanKitaplariListele()
+        {
+            return Kitaplar.Where(x => x.Durum == Durum.OduncVerildi).ToList();
+        }
+        public List<Uye> UyeleriListele()
+        {
+            return Uyeler;
+        }
+    }
+}
---
+++ CSProjeDemo1/Uye.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSProjeDemo1
+{
+    public class Uye : IUye
+    {
+        public string Ad { get ; set ; }
+        public string Soyad { get; set; }
+        public int UyeNo { get; set; }
+        public List<Kitap> OduncKitaplar { get; set; }
+
+        public Uye()
+        {
+            OduncKitaplar = new List<Kitap>();
+        }
+
+        public void KitapIadeEt(Kitap kitap)
+        {
+            if (OduncKitaplar.Contains(kitap))
+            {
+                kitap.Durum = Durum.OduncAlabilir;
+                OduncKitaplar.Remove(kitap);
+                Console.WriteLine($"{Ad} {Soyad} isimli üye,'{kitap.Baslik}' ");
+            }
+            else
+            {
+                Console.WriteLine("Bu kitap zaten sizde değil");
+            }
+        }
+
+        public void KitapOduncAl(Kitap kitap)
+        {
+            if(kitap.Durum==Durum.OduncAlabilir)
+            {
+                kitap.Durum = Durum.OduncVerildi;
+                OduncKitaplar.Add(kitap);
+                Console.WriteLine($"{Ad} {Soyad} isimli üye,'{kitap.Baslik}' ");
+            }
+            else
+            {
+                Console.WriteLine("Bu kitap ödünç alınamaz");
+            }
+        }
+    }
+}
